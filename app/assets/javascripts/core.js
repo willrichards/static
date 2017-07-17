1badb536aa7100157fdbcdc53553c38736c17e25
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2017-07-17 15:57:16 +0000

    Change search javascript to a module

--- app/assets/javascripts/core.js
+++ app/assets/javascripts/core.js
@@ -1,23 +1,6 @@
 $(document).ready(function() {
   $('.print-link a').attr('target', '_blank');
 
-  var $searchFocus = $('.js-search-focus');
-  $searchFocus.each(function(i, el) {
-    if($(el).val() !== '') {
-      $(el).addClass('focus');
-    }
-  });
-
-  $searchFocus.on('focus', function(e) {
-    $(e.target).addClass('focus');
-  });
-
-  $searchFocus.on('blur', function(e) {
-    if($(e.target).val() === '') {
-      $(e.target).removeClass('focus');
-    }
-  });
-
   if (window.GOVUK) {
     // for radio buttons and checkboxes
     var buttonsSelector = "label.selectable input[type='radio'], label.selectable input[type='checkbox']";
